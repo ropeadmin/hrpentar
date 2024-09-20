1bb19aa33449c4eb56679b991c2af757b6558edc
Godwin Olele <godwinolele10@gmail.com>
2024-09-20 21:38:30 +0100

    Welcome page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import Banner from '../components/Banner/Banner'
 
 export default function Dashboard() {
   return (
-    <div>
-      <Banner />
-    </div>
+    <div>Dashboard</div>
   )
 }
