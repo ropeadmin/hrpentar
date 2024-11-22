b7192082612511fedccf86a47e0bd9e78b2fa1a8
abiodun603 <abioduncsc@gmail.com>
2024-11-22 18:05:14 +0100

    fix: base route should route user to admin signin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,22 @@
 'use client';
 
-import useGlobalState from '@/hooks/globalstate.hook';
 import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
 import SplashScreen from './components/SplashScreen/SplashScreen';
 import { FadeIn } from './components/Transitions/Transitions';
-import EmployeeSignIn from './employee/(auth)/signin/page';
 
 export default function Home() {
   const navigate = useRouter();
-  const { isAuthenticated } = useGlobalState();
 
-  // useEffect(() => {
-  //   const timeoutId = setTimeout(() => {
-  //     isAuthenticated ? navigate.replace('/dashboard/welcome') : navigate.replace('/employee/signin');
-  //   }, 3000);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      navigate.replace('/auth/signin');
+    }, 3000);
 
-  //   return () => {
-  //     clearTimeout(timeoutId);
-  //   };
-  // }, []);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [navigate]);
 
   return (
     <FadeIn>
